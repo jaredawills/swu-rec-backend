be552bd8b11ad6ae4819b26bb63213abc0590991
Jared Wills <jaredwills@outlook.com>
2026-04-12 14:01:05 -0500

    Updated logo to link to home Cleaned up regex substitute statements into `replace_text`

--- html_writer.py
+++ html_writer.py
@@ -17,6 +17,7 @@ from loguru import logger
 def read_file(in_file):
     with open(in_file, 'rt', encoding='utf-8') as file:
         return file.read()
+  
     
 def write_file(out_file, text):
     out_file = Path(out_file)
@@ -24,20 +25,20 @@ def write_file(out_file, text):
     with open(out_file, 'wt', encoding='utf-8') as file:
         file.write(text)
 
-def text_replace(mapping, text):
-    for key, value in mapping.items:
+
+def replace_text(mapping, text):
+    for key, value in mapping.items():
         text = re.sub(key, value, text)
     return text
+
 
 def write_index():
     logger.info('Writing index')
     index_html = read_file('html_pieces/index.html')
     index_set_section = read_file('html_pieces/index_set_section.html')
     index_set_leader_article = read_file('html_pieces/index_set_leader_article.html')
-
     sets = db_conn.read('sets')
     cards = db_conn.read('cards')
-
     set_sections = []
     set_filter = []
     for set in sets.itertuples():
@@ -45,21 +46,23 @@ def write_index():
         if set_leaders.shape[0] > 0:
             leader_articles = []
             for leader in set_leaders.itertuples():
-                t = index_set_leader_article[:]
-                t = re.sub('%card_num', re.sub('_', '-', leader.card_id), t)
-                t = re.sub('%card_id', leader.card_id, t)
-                t = re.sub('%lower_title', leader.title.lower(), t)
-                t = re.sub('%title', leader.title, t)
-                t = re.sub('%subtitle', leader.subtitle, t)
-                t = re.sub('%back_art', leader.back_art, t)
-                t = re.sub('%set_code', leader.set_code, t)
-                leader_articles.append(t)
-            t = index_set_section[:]
-            t = re.sub('%set_code', set.set_code, t)
-            t = re.sub('%title', set.title, t)
-            t = re.sub('%leader_grid', '\n'.join(leader_articles), t)
+                sub_map = {
+                    '%card_num': re.sub('_', '-', leader.card_id),
+                    '%card_id': leader.card_id,
+                    '%lower_title': leader.title.lower(),
+                    '%title': leader.title,
+                    '%subtitle': leader.subtitle,
+                    '%back_art': leader.back_art,
+                    '%set_code': leader.set_code
+                }
+                leader_articles.append(replace_text(sub_map, index_set_leader_article[:]))
+            sub_map = {
+                '%set_code': set.set_code,
+                '%title': set.title,
+                '%leader_grid': '\n'.join(leader_articles),
+            }
+            set_sections.append(replace_text(sub_map, index_set_section[:]))
             set_filter.append(f'<option value=\"{set.set_code}\">{set.title}</option>')
-            set_sections.append(t)
     index_html = re.sub('%set_filter', '\n'.join(set_filter), index_html)
     index_html = re.sub('%set_sections', '\n'.join(set_sections), index_html)
     write_file('html\\index.html', index_html)
@@ -70,22 +73,22 @@ def get_leader_articles(card_grid=[], card_id=None):
         query = re.sub('%card_id', card_id, read_file('leader_query.sql'))  
         card_grid = db_conn.query(query)
     leader_card_article = read_file('html_pieces/leader_card_article.html')
-
     articles = []
     for card in card_grid.itertuples():
+        sub_map = {
+            '%card_type': card.card_type,
+            '%aspects': re.sub(',', ' ', card.aspects if card.aspects else ''),
+            '%set_code': card.set_code,
+            '%copy3': str(round(card.copy3 / card.tot_decks * 100, 2)),
+            '%copy3': str(round(card.copy3 / card.tot_decks * 100, 2)),
+            '%copy1': str(round(card.copy1 / card.tot_decks * 100, 2)),
+            '%front_art': card.front_art,
+            '%title': card.title,
+            '%subtitle': card.subtitle if card.subtitle else '',
+            '%card_num': re.sub('_', '-', card.card_id),
+        }
+        articles.append(replace_text(sub_map, leader_card_article[:]))
         t = leader_card_article[:]
-        t = re.sub('%card_type', card.card_type, t)
-        aspects = card.aspects if card.aspects else ''
-        t = re.sub('%aspects', re.sub(',', ' ', aspects), t)
-        t = re.sub('%set_code', card.set_code, t)
-        t = re.sub('%copy3', str(round(card.copy3 / card.tot_decks * 100, 2)), t)
-        t = re.sub('%copy2', str(round(card.copy2 / card.tot_decks * 100, 2)), t)
-        t = re.sub('%copy1', str(round(card.copy1 / card.tot_decks * 100, 2)), t)
-        t = re.sub('%front_art', card.front_art, t)
-        t = re.sub('%title', card.title, t)
-        subtitle = card.subtitle if card.subtitle else ''
-        t = re.sub('%subtitle', subtitle, t)
-        t = re.sub('%card_num', re.sub('_', '-', card.card_id), t)
         articles.append(t)
     return '\n'.join(articles)
 
@@ -94,30 +97,26 @@ def write_set_leader_pages(sets, cards, set_code):
     leaders = cards[(cards['set_code']==set_code) & (cards['card_type']=='Leader')].sort_values('card_id')
     leader_html = read_file('html_pieces/leader.html')
     for leader in leaders.itertuples():
+        logger.debug(f'Writing {leader.card_id}')
         card_grid_query = re.sub('%card_id', leader.card_id, read_file('leader_query.sql'))  
         card_grid = db_conn.query(card_grid_query)
-        set_filters = [f'<option value=\"{set.set_code}\">{set.title}</option>' 
-                        for set in sets.itertuples()
-                        if set.set_code in card_grid['set_code'].drop_duplicates().values]
-        logger.debug(f'Writing {leader.card_id}')
-        t = leader_html[:]
-        t = re.sub('%title', leader.title, t)
-        set_title = sets[sets['set_code']==set_code]['title'].values[0]
-        t = re.sub('%set_title', str(set_title), t)
-        t = re.sub('%set_code', leader.set_code, t)
-        t = re.sub('%subtitle', leader.subtitle, t)
-        t = re.sub('%card_num', re.sub('_', '-', leader.card_id), t)
-        t = re.sub('%aspects', re.sub(',', ' ', leader.aspects), t)
-        t = re.sub('%traits', ' '.join([t.title() for t in leader.traits.split(',')]), t)
-        t = re.sub('%front_text', leader.front_text, t)
-        t = re.sub('%back_text', leader.back_text, t)
-        t = re.sub('%front_art', leader.front_art, t)
-        t = re.sub('%back_art', leader.back_art, t)
-        t = re.sub('%set_filters', '\n'.join(set_filters), t)
-        t = re.sub('%card_grid', get_leader_articles(card_grid=card_grid), t)
-        deck_count = db_conn.query(f'SELECT COUNT(*) FROM deck_leaders WHERE card_id = \'{leader.card_id}\'').values[0][0]
-        t = re.sub('%decks', str(deck_count), t)
-        write_file(f'html/{leader.set_code}/{leader.card_id}.html', t)
+        sub_map = {
+            '%title': leader.title,
+            '%subtitle': leader.subtitle,
+            '%set_title': sets[sets['set_code']==set_code]['title'].values[0],
+            '%set_code': set_code,
+            '%card_num': re.sub('_', '-', leader.card_id),
+            '%aspects': re.sub(',', ' ', leader.aspects),
+            '%traits': ' '.join([t.title() for t in leader.traits.split(',')]),
+            '%front_text': leader.front_text,
+            '%back_text': leader.back_text,
+            '%front_art': leader.front_art,
+            '%back_art': leader.back_art,
+            '%set_filters': '\n'.join([f'<option value=\"{set.set_code}\">{set.title}</option>' for set in sets.itertuples() if set.set_code in card_grid['set_code'].drop_duplicates().values]),
+            '%card_grid': get_leader_articles(card_grid=card_grid),
+            '%decks': str(db_conn.query(f'SELECT COUNT(*) FROM deck_leaders WHERE card_id = \'{leader.card_id}\'').values[0][0])
+        }
+        write_file(f'html/{leader.set_code}/{leader.card_id}.html', replace_text(sub_map, leader_html[:]))
         
     
     
