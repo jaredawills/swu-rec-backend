99f32aeb0ecd7727dd434907161f839603639dd7
JAW17000 <jaredwills@outlook.com>
2026-04-15 22:08:25 -0500

    updated daily script

--- swu-rec_daily.sh
+++ swu-rec_daily.sh
@@ -1,21 +1,23 @@
+# Fetch swu-rec-backend
 git fetch --all
 git reset --hard origin/main
-source ../.swu-rec/bin/activate
-python3 main.py
-git add .
-git commit -m "Daily Update"
-git push origin
+
+# Fetch swu-rec (HTML)
 cd html
-git add .
-git commit -m "Daily Update"
-git push origin
 git fetch --all
 git reset --hard origin/main
+cd ..
+
+# Run program
 source ../.swu-rec/bin/activate
 python3 main.py
+
+# Commit swu-rec-backend
 git add .
 git commit -m "Daily Update"
 git push origin
+
+# Commit swu-rec (HTML)
 cd html
 git add .
 git commit -m "Daily Update"
