a5e8fce36cc8df7d5c96b80c0f4fc2e91d4a004b
Jared Wills <jaredwills@outlook.com>
2026-08-27 23:58:35 -0500

    spcecified push to origin

--- src/swu_rec_git/__init__.py
+++ src/swu_rec_git/__init__.py
@@ -19,7 +19,8 @@ def push(repo):
     if len(staged_diffs) > 0:
         today = date.today().strftime('%Y-%m-%d')
         repo.index.commit(f"Daily Update - {today}")
-        repo.push()
+        origin = repo.remote(name="origin")
+        origin.push()
 
 def clone():
     repo_url = "https://github.com/jaredawills/swu-rec"
